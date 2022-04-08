c5d87581c0ba3da7364440dd6a61b77846b217ee
User <User@172.16.212.117>
2022-04-07 22:39:21 -0500

    Ultimos cambios

--- src/main/java/com/mycompany/clase4/Auto.java
+++ src/main/java/com/mycompany/clase4/Auto.java
@@ -1,21 +1,12 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package com.mycompany.clase4;
 
-/**
- *
- * @author User
- */
-public class Auto {
-    public double calcularMatricula(){
+  package com.mycompany.clase4;      
+   
+public class Auto{
     String marca;
     String color;
     int year;
     double precio ;
     String placa;
-    
     public boolean esTaxi(){
         var retorno = false;
          if(this.color=="amarillo"){
@@ -23,7 +14,6 @@ public class Auto {
          }
         return retorno;
     }
-    
     public int calcularTasaSolidaria(){
         var retorno= 1000000;
         if(this.precio>0 && this.precio<1000){
@@ -42,35 +32,52 @@ public class Auto {
          }
         return retorno;        
     }
-    
-     public double calcularMatricula(){
+    public double calcularMatricula(int limityear,int limitprecio){
         var retorno= 10000d;
-        if(this.year>=0 && this.year<=2010){
-            if (this.precio>=0&&this.precio<=10000){
-                
-        }else{
-            if(this.precio>=1001 && this.precio<10000){
-                retorno=100;
-            }  
-            else{
-                if(this.precio>=1001 && this.precio<25000){
-                    retorno=250;
-                } else{
-                    retorno =370;
-                }   
+        if(this.year>=0 && this.year<=limityear){
+            if(this.precio>=0 && this.precio<=limitprecio)
+                retorno=this.precio*0.1;
+            else
+                retorno=this.precio*0.2;
+         }else{
+                if(this.precio>=0 && this.precio<=limitprecio)
+                    retorno=this.precio*0.15;
+                else
+                    retorno=this.precio*0.25;
+              
             }
-            
-            
-         }
-        
-        
         return retorno;        
-        }
-     
-            
+    }
+    public String obtenerProvincia(){
+         var retorno="TBD";
+         var primerCaracterPlaca=this.placa.charAt(0);
+         switch (primerCaracterPlaca)
+         {
+           case'A':
+                 retorno="Azuay";
+                 break;
+            case'B':
+                 retorno="Bolivar";
+                 break;
+            case'C':
+                 retorno="Carchi";
+                 break;
+            case'U':
+                 retorno="Cañar";
+                 break;
+            case'G':
+                 retorno="Guayas";
+                 break;
+            case'P':
+                 retorno="Pichincha";
+                 break;
+            default:
+                retorno="Provincia no disponible";
+         }
+         return retorno;
      }
-    
-}
+    public int calcularYear(int currentyear){
 
-        
-      
+         return currentyear - this.year;
+     }
+}
--- src/main/java/com/mycompany/clase4/Clase4.java
+++ src/main/java/com/mycompany/clase4/Clase4.java
@@ -13,5 +13,65 @@ public class Clase4 {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        var auto1 = new Auto();
+        auto1.color="rojo";
+        auto1.marca="Hyundai";
+        auto1.year=2020;
+        auto1.placa="AGH8001";
+        auto1.precio= 20999d;
+        ////////////////////////////////////////////
+        System.out.println(auto1.color+ " / "+
+                           auto1.marca+ " / "+
+                           auto1.year+ " / "+
+                           auto1.placa+ " / "+
+                           auto1.precio+ " / ");
+        //////////////////////////////////////////
+        var esTaxi = false;
+        esTaxi=auto1.esTaxi();
+        if(esTaxi==true){
+        System.out.println( "El auto de placa " + auto1.placa + " es un taxi.");
+        }else{
+              System.out.println( "El auto de placa " + auto1.placa + " No es un taxi.");
+        }
+        ////////////////////////////////////////////// 
+        System.out.println( "El auto con precio " + auto1.precio + " debe pagar la tasa solidario de " 
+                                + auto1.calcularTasaSolidaria());
+        /////////////////////////////////////////////  
+       // var costoMatricula=10000d;
+         var costoMatricula= auto1.calcularMatricula(2010,10000);
+          System.out.println("Debe pagar"+costoMatricula+"USD pro concepto de matricula");
+          //////////////////////////////////////////
+          System.out.println("El auto es de la provincia de: "+auto1.obtenerProvincia());
+          
+        var actualYear=2022;
+        actualYear= auto1.calcularYear(actualYear);
+          System.out.println("El auto tiene: " + actualYear+"años");
+        /////////////////////////////////////////
+        var auto2 = new Auto();
+        auto2.color="amarillo";
+        auto2.marca="Chevrolet";
+        auto2.year=2021;
+        auto2.placa="GBF8087";
+        auto2.precio= 22808d;
+        
+        System.out.println(auto2.color+ " / "+
+                           auto2.marca+ " / "+
+                           auto2.year+ " / "+
+                           auto2.placa+ " / "+
+                           auto2.precio+ " / ");
+ 
+        esTaxi=auto2.esTaxi();
+        if(esTaxi==true){
+            System.out.println( "El auto de placa " + auto2.placa + " es un taxi.");
+        }else{
+            System.out.println( "El auto de placa " + auto2.placa + " No es un taxi.");
+         } 
+          System.out.println( "El auto con precio " + auto2.precio + " debe pagar la tasa solidario de " 
+                                + auto2.calcularTasaSolidaria());
+        
+          costoMatricula= auto2.calcularMatricula(2010,10000);
+          System.out.println("Debe pagar"+costoMatricula+"USD pro concepto de matricula");
+          
+          System.out.println("El auto es de la provincia de: "+auto2.obtenerProvincia());
     }
 }
